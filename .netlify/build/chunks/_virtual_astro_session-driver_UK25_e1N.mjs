1b02bb08709dbcfb620d3df6215c7ce698d3a562
Krisztina Závecz <krisztina.zavecz@memoq.com>
2026-08-21 16:18:15 +0200

    cleanup

--- .netlify/build/chunks/_virtual_astro_session-driver_UK25_e1N.mjs
+++
@@ -1,6 +0,0 @@
-import _default from "unstorage/drivers/netlify-blobs";
-export * from "unstorage/drivers/netlify-blobs";
-//#region \0virtual:astro:session-driver
-var _virtual_astro_session_driver_default = _default;
-//#endregion
-export { _virtual_astro_session_driver_default as default };
