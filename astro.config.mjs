7781c3c1d68a8c1d95d25b0cab3b1f00ba5707fe
Krisztina Závecz <krisztina.zavecz@memoq.com>
2026-08-21 16:04:25 +0200

    fix site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://krisztinazavecz.com",
+  integrations: [mdx(), sitemap()],
+  adapter: netlify(),
 });
