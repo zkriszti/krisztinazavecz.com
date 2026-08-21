2f1db56337b8921552689cc6af9458ac5d9935c8
Krisztina Závecz <krisztina.zavecz@memoq.com>
2026-08-21 14:57:54 +0200

    migrate page to Astro - initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE =
+  "Krisztina Závecz - Frontend Development - HTML, CSS, JavaScript and related technologies";
+export const SITE_DESCRIPTION =
+  "Krisztina Závecz | Frontend Developer from Budapest, Hungary";
