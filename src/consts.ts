2f4953fc644893c2acc7f08d3e54b2d96904060c
Krisztina Závecz <krisztina.zavecz@memoq.com>
2026-08-22 23:24:39 +0200

    handle site title / description, no meta img at this point

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE =
-  "Krisztina Závecz - Frontend Development - HTML, CSS, JavaScript and related technologies";
-export const SITE_DESCRIPTION =
   "Krisztina Závecz | Frontend Developer from Budapest, Hungary";
+export const SITE_DESCRIPTION =
+  "A personal website by Krisztina Závecz. Read about the 'front of the frontend': HTML, CSS, JavaScript and related topics.";
